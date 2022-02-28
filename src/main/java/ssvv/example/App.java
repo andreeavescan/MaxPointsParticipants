b24d708ab782edb1d52a0eca533465bbac572591
Andreea <andreea.vescan@gmail.com>
2022-02-28 15:10:14 +0200

    Initial commit

---
+++ src/main/java/ssvv/example/App.java
@@ -0,0 +1,22 @@
+package ssvv.example;
+//package Lecture02MaxPointsParticipantsMV;
+import controller.ParticipantController;
+import repository.Repository;
+import ui.ParticipantUI;
+
+
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        String participantsFile = "FileParticipants.txt";
+
+        Repository repo = new Repository(participantsFile);
+        ParticipantController ctrl = new ParticipantController(repo);
+
+        ParticipantUI console = new ParticipantUI(ctrl);
+        console.Run();
+    }
+}
